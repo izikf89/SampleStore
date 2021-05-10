857cae575dffcd8d6e24750628e84a44935a66cf
Michael Fisher <michael.fisher6042@cs.colman.ac.il>
2021-05-10 22:52:34 +0300

    Commit Michael

---
+++ WebApplication/Migrations/20210510194416_initall.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class initall : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Product",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Product",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    AccountId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    FullName = table.Column<string>(nullable: true),
+                    Mail = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.AccountId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<int>(nullable: false),
+                    Total = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_OrderId",
+                table: "Product",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Order_OrderId",
+                table: "Product",
+                column: "OrderId",
+                principalTable: "Order",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Order_OrderId",
+                table: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_OrderId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Product");
+        }
+    }
+}
---
+++ WebApplication/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int ClientId { get; set; }
+        public List<Product> ListOfProducts { get; set; }
+        public int Total { get; set; }
+    }
+}
